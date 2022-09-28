e935c85656a3eb95ee57ea244b555ab43b37083c
FL3TCH0R <114429899+FL3TCH0R@users.noreply.github.com>
2022-09-28 13:25:54 -0400

    Add files via upload

---
+++ Scanneroperators.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.scanneroperators;
+
+/**
+ *
+ * @author fletc
+ */
+public class Scanneroperators { 
+    
+    
+
+    public static void main(String[] args) {
+        
+        int score= 20;
+        if(score==90)
+        {
+            System.out.println("score is 90%");
+        }
+        
+            else if(score==80)
+        
+        {
+            System.out.println("score is 80%");
+        }
+        else
+        {
+                System.out.println("Grade is below 40");
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
