e44087179c015a5f80a5de928b15a04cd81bb51c
FL3TCH0R <114429899+FL3TCH0R@users.noreply.github.com>
2022-09-28 14:18:29 -0400

    Add files via upload

---
+++ Importscanner.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.importscanner;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fletc
+ */
+public class Importscanner {
+
+    public static void main(String[] args) {
+       
+      Scanner input= new Scanner(System.in);
+      
+      System.out.println("Enter your name");
+      
+        
+    //String name= input.nextLine();
+    //String name = input.next();
+    int name =input.nextInt();
+    System.out.println("My name is"+name);
+    
+    Scanner input1 = new Scanner(System.in);
+    
+    System.out.println("Enter an integer");
+    int num1=input1.nextInt();
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
